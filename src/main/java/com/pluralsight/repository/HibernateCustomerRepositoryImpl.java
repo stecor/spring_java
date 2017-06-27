dff02011949d8f288afa809badc93f6ffb7c7561
Stefano Corra <stefano.corra@hotmail.com>
2017-06-27 18:05:38 -0400

    second commit

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,13 +3,14 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.pluralsight.model.Customer;
 
+@Repository("customerRepositorys")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
-	/* (non-Javadoc)
-	 * @see com.pluralsight.repository.CustomerRepository#findAll()
-	 */
+
 	@Override
 	public List<Customer> findAll(){
 		List<Customer> customers = new ArrayList<>();
